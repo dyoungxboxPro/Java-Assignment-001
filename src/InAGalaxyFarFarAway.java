3d4dc24458b4de4d884ad6aeec1e1cb2cd82b401
dyoungxboxpro <dyoung367@mycr.redwoods.edu>
2023-09-24 19:03:46 -0700

    I had to Redo This lab beacuase it disapeared for some reason.

--- src/InAGalaxyFarFarAway.java
+++ src/InAGalaxyFarFarAway.java
@@ -1,9 +1,18 @@
+/**
+ *
+ *
+ * @author Tervor Hartman
+ * @author Devin Young
+ *
+ * @Since Version 1.0
+ */
 public class InAGalaxyFarFarAway {
     /**
      * Prints a famous Star Wars intro to the console with 3 souts.
      * @param args Command line arguments [The source file path, The target file path, ...]
      **/
     public static void main(String[] args) {
-        // Write your program here
+        System.out.println("Along Time Ago In A Galaxy Far Far Away");
+
     }
 }
